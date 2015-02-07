4f783b1096e3eb1df5526279b7e972f52c9c676d
sjas <sjas@nb.ss.dz>
2015-02-07 04:08:40 +0100

    tempcommit

--- init.el
+++ init.el
@@ -1,7 +1,16 @@
+;;;
 ;;; init.el --- sjas .emacs initialization file
 
 ;;; Commentary:
-;;; no info yet.
+;;; Now all makes sense.
+
+;;; TODO
+;;;  * check vim search&replace syntax to work properly (evil-mode)
+;;;  * fix the autocomplete plugin mess (AC, company, whatever)
+;;;  * check where the delay for AC is set
+;;;  * rebind 'mark here' and make AC work with C-SPC
+;;;  * fix flycheck-next/prev to start search at beginning when reaching end
+
 ;;; 1/2015
 
 ;;; Code:
@@ -423,9 +432,11 @@
 (define-key flycheck-mode-map flycheck-keymap-prefix (kbd "C-c f"))
 (define-key flycheck-mode-map flycheck-keymap-prefix flycheck-command-map)
 
-(evil-leader/set-key "j" 'flycheck-next-error)
-(evil-leader/set-key "k" 'flycheck-previous-error)
+(evil-leader/set-key "j" (lambda () (interactive) (flycheck-next-error t)))
+(evil-leader/set-key "k" (lambda () (interactive) (flycheck-previous-error)))
 (evil-leader/set-key "l" 'flycheck-list-errors)
 
 (provide 'init)
 ;;; init.el ends here
+
+;;;   
